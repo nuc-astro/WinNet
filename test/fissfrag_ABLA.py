4be87da460679a0af5baee2dfd2568c002bb4fb4
MReichert91 <mreichert@theorie.ikp.physik.tu-darmstadt.de>
2023-08-04 13:50:51 +0200

    (-) ABLA test case

--- test/fissfrag_ABLA.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-t.checklist = { \
-   'finab.dat'    : { 'method':'default', 'tolerance':1e-4 }, \
-}
-t.program = t.basedir + "/bin/winnet"
-t.testdir = t.basedir + "/test/" + t.testname
-t.logfile = t.testdir + ".log"
-t.arcfile = t.testdir + ".tar.gz"
