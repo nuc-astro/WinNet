f5bc806c09066c05a02156c89563bd9d33cbcfc8
MReichert91 <mreichert@theorie.ikp.physik.tu-darmstadt.de>
2023-10-18 11:45:38 +0200

    (*) tolerance for eq_emission test
    WIth the timestep changes the finab of this test changed slightly with
    gfortran. Therefore the tolerance increased from 5e-4 to 1e-3

--- test/emission_eq.py
+++ test/emission_eq.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 t.checklist = { \
-   'finab.dat'    : { 'method':'default', 'tolerance':5e-4 }, \
+   'finab.dat'    : { 'method':'default', 'tolerance':1e-3 }, \
 }
 t.program = t.basedir + "/bin/winnet"
 t.testdir = t.basedir + "/test/" + t.testname
